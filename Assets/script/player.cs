1ed729084e7905fcd53564dd9ab3002156d719f8
ohmanurak <54495874+ohmanurak@users.noreply.github.com>
2021-10-10 20:52:00 +0700

    Initial commit

---
+++ Assets/script/player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)){
+            animator.SetBool("isdribling",true);
+        }
+
+        if (Input.GetKey(KeyCode.D)){
+            animator.SetBool("isdribling",false);
+        }
+
+
+
+    }
+}
